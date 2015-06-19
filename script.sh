5fcf716df19a359518143fc6aaec904a31d6dc36
Laavanya <laavanya14@gmail.com>
2015-06-20 00:16:14 +0800

    added -y for auto install

--- script.sh
+++ script.sh
@@ -6,6 +6,6 @@ sudo apt-get -y install apache2
 
 sudo apt-get -y install php5 libapache2-mod-php5 php5-mcrypt
 
-sudo apt-get install openjdk-7-jre unzip
+sudo apt-get -y install openjdk-7-jre unzip
 wget "http://download.go.cd/gocd-deb/go-server-15.1.0-1863.deb"
 sudo dpkg -i go-server-15.1.0-1863.deb
